80277c7516640bb6e148c9a9ae3f8c0a6d91c787
Yash <kudveyash@gmail.com>
2024-02-28 17:49:13 +0530

    Added more question and modal

--- client/src/App.js
+++ client/src/App.js
@@ -5,13 +5,15 @@ import RoomPage from "./screens/Room";
 import DepressionTest from "./questions/Depress.jsx";
 import Depress1 from "./questions/Depress1.jsx";
 import Depress2 from "./questions/Depress2.jsx";
+import Depress3 from "./questions/Depress3.jsx";
 
 function App() {
   return (
     <div className="App">
       {/* <DepressionTest /> */}
       {/* <Depress1 /> */}
-      <Depress2 />
+      {/* <Depress2 /> */}
+      <Depress3 />
       {/* <Routes>
         <Route path="/" element={<LobbyScreen />} />
         <Route path="/room/:roomId" element={<RoomPage />} />
